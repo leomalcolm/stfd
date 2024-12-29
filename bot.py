d112b8b9d65c2ab1e2d089828c8f0819a9579980
leomalcolm <141685811+leomalcolm@users.noreply.github.com>
2024-12-30 00:14:04 +1300

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import os
 # Your bot token and channel ID
 TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
+CHA = 1322881877333508118
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -57,7 +58,7 @@ async def check_for_updates():
                 if pairings != last_pairings:
                     # If new pairings are detected, update the stored pairings and notify on Discord
                     last_pairings = pairings
-                    new_pairing_message = f":bangbang: **{intro_message}** @championship :bangbang:\n\n{pairings}"
+                    new_pairing_message = f":bangbang: **{intro_message}** <@&{CHA}> :bangbang:\n\n{pairings}"
 
                     # Send the message to Discord
                     channel = client.get_channel(int(CHANNEL_ID))
