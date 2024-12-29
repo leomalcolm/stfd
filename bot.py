5fa07f8464fdfa5dd2b88c8d9a23473ad8957bcc
leomalcolm <141685811+leomalcolm@users.noreply.github.com>
2024-12-29 23:18:23 +1300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,34 +1,62 @@
 import discord
-from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
+import asyncio
 import os
 
-# Set up intents (Make sure to enable 'Message Content Intent' in the Developer Portal)
+# Your bot token and channel ID
+TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
+
 intents = discord.Intents.default()
-intents.message_content = True  # This line enables reading message content
+client = discord.Client(intents=intents)
 
-# Create bot
-bot = commands.Bot(command_prefix="!", intents=intents)
+# Define the Vega URL for checking pairings
+VEGA_URL = "https://newzealandchess.co.nz/tournaments/misc/2024/www2024%20New%20Zealand%20Championship/pairs9.html"
 
-# Event: Bot is ready and sends a message
-@bot.event
+# Store the last set of pairings to avoid redundant notifications
+last_pairings = ""
+
+# Function to check for updates
+async def check_for_updates():
+    global last_pairings
+    
+    try:
+        # Send a GET request to the page
+        response = requests.get(VEGA_URL)
+
+        if response.status_code == 200:
+            # Parse the HTML content
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Find the pairing data (we assume the pairings are within <pre> tags, adjust as needed)
+            pairings_section = soup.find_all('pre')
+
+            if pairings_section:
+                # Get the text content of the pairings (this might need adjustment)
+                pairings = pairings_section[0].get_text(strip=True)
+                
+                # Check if the pairings have changed
+                if pairings != last_pairings:
+                    # If new pairings are detected, update the stored pairings and notify on Discord
+                    last_pairings = pairings
+                    new_pairing_message = "New round pairings are available:\n" + pairings
+
+                    # Send the message to Discord
+                    channel = client.get_channel(int(CHANNEL_ID))
+                    await channel.send(new_pairing_message)
+
+    except Exception as e:
+        print(f"Error fetching updates: {e}")
+
+# When the bot is ready, start checking for updates
+@client.event
 async def on_ready():
-    print(f"Logged in as {bot.user}!")
-    
-    # Get the channel you want to send the message to (use the channel ID)
-    channel_id = 1185784461674151950
-    channel = bot.get_channel(channel_id)
-    
-    # Send a message to the channel
-    if channel:
-        await channel.send("Hello! I'm online and ready to go!")
-    else:
-        print("Couldn't find the channel to send the message!")
-
-# Command: Ping
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong!")
+    print(f'Logged in as {client.user}')
+    # Check every 60 seconds (adjust as needed)
+    while True:
+        await check_for_updates()
+        await asyncio.sleep(60)
 
 # Run the bot
-TOKEN = os.getenv("DISCORD_TOKEN")
-bot.run(TOKEN)
+client.run(TOKEN)
