65ada817a488ab004039d36acc3fc6aebf64788d
leomalcolm <141685811+leomalcolm@users.noreply.github.com>
2024-12-29 23:03:35 +1300

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,17 +2,27 @@ import discord
 from discord.ext import commands
 import os
 
-# Set up intents (required for modern bots)
+# Set up intents
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Bot setup
+# Create bot
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Event: Bot is ready
+# Event: Bot is ready and sends a message
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}!")
+    
+    # Get the channel you want to send the message to (use the channel ID)
+    channel_id = 123456789012345678  # Replace this with your actual channel ID
+    channel = bot.get_channel(channel_id)
+    
+    # Send a message to the channel
+    if channel:
+        await channel.send("Hello! I'm online and ready to go!")
+    else:
+        print("Couldn't find the channel to send the message!")
 
 # Command: Ping
 @bot.command()
