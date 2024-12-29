e591f33c20aa79ef2b1b2b4fdcacaef492ad6448
leomalcolm <141685811+leomalcolm@users.noreply.github.com>
2024-12-29 23:09:14 +1300

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ import discord
 from discord.ext import commands
 import os
 
-# Set up intents
+# Set up intents (Make sure to enable 'Message Content Intent' in the Developer Portal)
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True  # This line enables reading message content
 
 # Create bot
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -15,7 +15,7 @@ async def on_ready():
     print(f"Logged in as {bot.user}!")
     
     # Get the channel you want to send the message to (use the channel ID)
-    channel_id = 123456789012345678  # Replace this with your actual channel ID
+    channel_id = 1185784461674151950
     channel = bot.get_channel(channel_id)
     
     # Send a message to the channel
